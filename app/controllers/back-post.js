97a6f0a7e5b15fe8be0c078d715daef17ccabc3c
Alejandro de Haro <a.deharo9@gmail.com>
2019-10-12 19:02:01 +0200

    Deletes logging

--- app/controllers/back-post.js
+++ app/controllers/back-post.js
@@ -2,6 +2,5 @@ const genericPost = require('./generic-post');
 
 module.exports = (body) =>
 {
-    console.log(process.pid);
     return genericPost(`./scripts/deploy-back ${process.pid}`, body);
 };
